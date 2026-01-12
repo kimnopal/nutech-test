cb1a17024666553a3a7df810c628501912264dd0
kimnopal <naufalhakim366@gmail.com>
2026-01-12 23:54:00 +0700

    fix: add base url on swagger config

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -20,7 +20,7 @@ const options: swaggerJsdoc.Options = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 3000}`,
+        url: `${process.env.BASE_URL}`,
         description: "Development server",
       },
     ],
